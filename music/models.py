7b888d5921646908b81b9c3715000c2bc602f8dd
maddymaster <maddymaster@gmail.com>
2016-09-23 21:05:08 +0530

    Second Commit
    Final Commit

--- music/models.py
+++ music/models.py
@@ -1,11 +1,14 @@
+from django.contrib.auth.models import Permission, User
 from django.db import models
 
 
 class Album(models.Model):
-    artist = models.CharField(max_length=300)
-    album_title = models.CharField(max_length=300)
-    genre = models.CharField(max_length=50)
-    album_logo = models.CharField(max_length=1000)
+    user = models.ForeignKey(User, default=1)
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.FileField()
+    is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
         return self.album_title + ' - ' + self.artist
@@ -13,8 +16,8 @@ class Album(models.Model):
 
 class Song(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
-    file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
+    audio_file = models.FileField(default='')
     is_favorite = models.BooleanField(default=False)
 
     def __str__(self):
